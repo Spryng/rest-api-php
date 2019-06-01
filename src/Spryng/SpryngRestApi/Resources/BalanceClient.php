065913e881d19df612bd946d06a6c54499ed7504
Roemer Bakker <contact@roemerbakker.com>
2019-06-01 17:01:29 +0200

    Implemented get balance endpoint

--- src/Spryng/SpryngRestApi/Resources/BalanceClient.php
+++ src/Spryng/SpryngRestApi/Resources/BalanceClient.php
@@ -3,8 +3,19 @@
 namespace Spryng\SpryngRestApi\Resources;
 
 use Spryng\SpryngRestApi\BaseClient;
+use Spryng\SpryngRestApi\Http\HttpClient;
+use Spryng\SpryngRestApi\Http\Request;
 
 class BalanceClient extends BaseClient
 {
-
+    public function get()
+    {
+        return (new Request(
+            $this->api->getBaseUrl(),
+            HttpClient::METHOD_GET,
+            '/balance'
+        ))
+            ->withBearerToken($this->api->getApiKey())
+            ->send();
+    }
 }
